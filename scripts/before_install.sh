817c51b3a3e4f7aa2f6304781866ea963b71d7f9
AmarSiddi <amarnathsiddi@gmail.com>
2020-04-17 19:15:02 +0100

    modified scripts

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo yum update
-sudo yum install nodejs -y
-sudo yum install npm  -y
-sudo yum forever -g
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_13.x | sudo -E bash -
+sudo yum install -y nodejs
+sudo npm install forever -g
