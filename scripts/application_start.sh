d6d09066016fd4be810d1be5cefc1981083a587d
AmarSiddi <amarnathsiddi@gmail.com>
2020-04-17 20:17:03 +0100

    modified scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/spw/expense
 
+npm start
 #sudo npm run /App
-
-forever stopall
-forever start -c "node -r esm" Server.js
+#forever stopall
+#forever start -c "node -r esm" Server.js
