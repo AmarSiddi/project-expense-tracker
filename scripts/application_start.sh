5e51e899b22daa220b6b492d8b2225ff1f3af433
AmarSiddi <amarnathsiddi@gmail.com>
2020-04-17 20:19:20 +0100

    modified scripts

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /home/ec2-user/spw/expense
 
-npm start
+sudo npm update
+sudo npm start
 #sudo npm run /App
 #forever stopall
 #forever start -c "node -r esm" Server.js
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update
-sudo yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_13.x | sudo -E bash -
-sudo yum install -y nodejs
-sudo npm install forever -g
+#sudo yum install -y gcc-c++ make
+#curl -sL https://rpm.nodesource.com/setup_13.x | sudo -E bash -
+#sudo yum install -y nodejs
+#sudo npm install forever -g
