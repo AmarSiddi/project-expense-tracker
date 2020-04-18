58b995974f80e4297eef88e11212e6add8a83206
AmarSiddi <amarnathsiddi@gmail.com>
2020-04-18 05:22:00 +0100

    modified everything, removed reactjs app

--- expense/Server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var port = 9500;
-var server = http.createServer(function(request, response) {
-    response.setHeader('Content-Type', 'text/html');
-    response.end('<h1>Node.js server is running!</h1>');
-});
-server.listen(port, function() {
-    console.log('Node.js server listening on port: ' + port);
-});
