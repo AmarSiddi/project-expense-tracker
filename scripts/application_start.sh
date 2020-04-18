951082d7810ead54e16e786fa2e8e14822fbaba0
AmarSiddi <amarnathsiddi@gmail.com>
2020-04-18 19:23:40 +0100

    modified 'application_start.sh' script and jar location

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd  /home/ec2-user/project/
+cd  /home/ec2-user/project
+
+sudo /home/ec2-user/project/java -jar ExpenseTracker-0.0.1-SNAPSHOT.jar
+
+#sudo killall java
+#exit 0
 
 #sudo npm update
 #sudo npm start
@@ -7,7 +12,5 @@ cd  /home/ec2-user/project/
 #forever stopall
 #forever start -c "node -r esm" Server.js
 
-#sudo killall java
-#exit 0
 
-sudo java -jar ExpenseTracker-0.0.1-SNAPSHOT.jar
+
