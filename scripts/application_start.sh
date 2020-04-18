3932163f5b015be786d0cb44ae56c37617e15c25
AmarSiddi <amarnathsiddi@gmail.com>
2020-04-18 14:22:19 +0100

    updated appspec.yml and added java jar snapshot for spring boot execution.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-cd  /home/ec2-user/project/expense/
+cd  /home/ec2-user/project/script/
 
-sudo npm update
-sudo npm start
+#sudo npm update
+#sudo npm start
 #sudo npm run /App
 #forever stopall
 #forever start -c "node -r esm" Server.js
+
+#sudo killall java
+#exit 0
+
+java -jar ExpenseTracker-0.0.1-SNAPSHOT.jar
